c88eb8cee01662a4abbcf8d664d036b569ddd919
Jorge Delgado <jorgeadm4@gmail.com>
2022-09-26 19:37:38 -0500

    Safety commit

--- Mod3/Act3_1/main.cpp
+++ Mod3/Act3_1/main.cpp
@@ -1,75 +1,88 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 using namespace std;
-#define um std::unordered_map
 
 struct Node {
-  um<char, Node> children;
+    std::unordered_map<char, Node> children;
 };
 
 void insert(Node *root, std::string word) {
-  Node *current = root;
+    Node *current = root;
 
-  for (char letter : word)
-    current = &(current->children[letter]);
+    for (char letter : word)
+        current = &(current->children[letter]);
 }
 
-// bool exists(Node root, std::string word) { 
-//   Node current = root;
-
-//   for (char letter : word) {
-//     if (current.children)
-//     current = current.children[letter];
-//   }
-// }
-
-void DFS(Node root) {
-    if (root.children.size() == 0) return;
-    for (auto child : root.children) {
-        std::cout << child.first << " ";
-        DFS(root.children[child.first]);
+bool exists(Node* root, std::string word) {
+    Node* current = root;
+    
+    for (char letter : word) {
+        if (!(current -> children.count(letter))) return false;
+        current = &(current -> children[letter]);
     }
+
+    return true;
+}
+
+void DFS(Node root, int spaces) {
+    if (root.children.size() == 0) {
+        for (int i = 0; i < spaces; i++)
+            std::cout << " ";
+        return;
+    }
+
+    //for (it = root.children.cend(); it != root.children.cbegin(); it++)
+    for (auto child : root.children)
+    {
+        std::cout << child.first;
+        DFS(root.children[child.first], spaces + 1);
+    }
+
     std::cout << endl;
-} 
+}
 
 int main() {
-  int n;
-  std::cin >> n;
+    int n, m;
+    std::cin >> n;
 
-  std::vector<std::string> words(n);
+    std::vector<std::string> words(n);
 
-  for (int i = 0; i < n; ++i)
-    std::cin >> words[i];
+    // for (int i = n - 1; i >= 0; --i)
+    //     std::cin >> words[i];
 
-  // int m;
-  // std::cin >> m;
+    for (int i = 0; i < n; ++i)
+        std::cin >> words[i];
 
-  // std::vector<std::string> queries(m);
+    // for (int i = n - 1; i >= 0; --i)
+    //     std::cin >> words[i];
 
-  // for(int i = 0; i < m; ++i)
-  //     std::cin >> queries[i];
+    // std::cin >> m;
 
-  Node root;
+    // std::vector<std::string> queries(m);
 
-  for (std::string word : words) {
-    insert(&root, word);
-  }
+    // for(int i = 0; i < m; ++i)
+    //     std::cin >> queries[i];
 
-  // for (auto child : root.children) {
-  //   std::cout << child.first << std::endl;
+    Node root;
 
-  //   for (auto child2 : root.children[child.first].children) {
-  //     std::cout << "\t" << child2.first << std::endl;
+    for (std::string word : words)
+        insert(&root, word);
 
-  //     for (auto child3 :
-  //          root.children[child.first].children[child2.first].children) {
-  //       std::cout << "\t\t" << child3.first << std::endl;
-  //     }
-  //   }
-  // }
-  Node current = root;
-  DFS(current);
-  return 0;
+    Node current = root;
+    //DFS(current, 0);
+
+    for (auto child : root.children) {
+        cout << child.first << endl;
+        for(auto child2 : root.children[child.first].children){
+            cout << "\t" << child2.first << endl;
+            for(auto child3 : root.children[child.first].children[child2.first].children) {
+                cout << "\t\t" << child3.first << endl;
+            }
+        }
+    }
+
+    return 0;
 }
