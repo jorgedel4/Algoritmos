26d51f12c911335b2f1d78e50a6574910d2b3894
Karla Sanchez <ksoalianza@gmail.com>
2022-11-03 13:05:18 -0600

    avance 4.3

---
+++ Mod4/Act4_3/main2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "Point.hpp"
+
+Point parseLine(string line) {
+    size_t divisor = line.find(", ");
+    int x, y;
+    x = stoi(line.substr(0, divisor));
+    y = stoi(line.substr(divisor + 2, line.length() - divisor - 2));
+
+    return Point(x, y);
+}
+
+int main() {
+    int n;
+    vector<Point> points(n);
+
+    string line;
+    for(int i = 0; i < n; ++i) {
+        getline(cin, line);
+        points[i] = parseLine(line);
+    }
+
+    for(Point point : points) {
+        point.print();
+    }
+
+    return 0;
+}
