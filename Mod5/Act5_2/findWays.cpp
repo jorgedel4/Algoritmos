a63acbc4f79d63d78730c7b5b8a4a7dbe7fc51f4
Jorge Delgado <jorgeadm4@gmail.com>
2022-11-16 19:47:12 -0600

    [5.2] Added time complexities

--- Mod5/Act5_2/findWays.cpp
+++ Mod5/Act5_2/findWays.cpp
@@ -10,16 +10,9 @@ int allUsedMask;
 
 //
 int countWays(int people, int hatTypes, int hatIndex, int bitmask, vector<vector<int>>& dp, unordered_map<int, vector<int>> personPerHat, vector < vector <int> >& answers, vector <int> options) {
-    // cout << "===================" << endl;
-    // cout << "hatIndex: " << hatIndex << endl;
-    // cout << "bitmask: " << bitmask << endl;
     // All people are wearing unique hats
     if (bitmask == allUsedMask) {
         answers.push_back(options); 
-        // cout << "===================" << endl;
-        // cout << "hatIndex: " << hatIndex << endl;
-        // cout << "bitmask: " << bitmask << endl; //esta es la persona que esta usando que gorra
-        // cout << "All people are wearing caps!" << endl;
         return 1;
     }
 
@@ -27,13 +20,11 @@ int countWays(int people, int hatTypes, int hatIndex, int bitmask, vector<vector
     if (hatIndex > hatTypes) return 0;
 
     // ---- FIRST OPTION ----
-    // This cap is skipped
+    // This cap is skipped 
     int ways = countWays(people, hatTypes, hatIndex+1, bitmask, dp, personPerHat, answers, options);
 
     // ---- SECOND OPTION ----
     // Allow this cap to a person
-    // hat 1 : person2, 10
-    // options [x, x, 1]
     for (int person : personPerHat[hatIndex]) {
         // Check that current person is not wearing a mask
         if ( ((1 << person) & bitmask) == 0) {
@@ -42,7 +33,6 @@ int countWays(int people, int hatTypes, int hatIndex, int bitmask, vector<vector
             ways += countWays(people, hatTypes, hatIndex+1, newMask, dp, personPerHat, answers, options);
         }
     }
-
 
     dp[bitmask][hatIndex] = ways;
     return ways;
@@ -67,13 +57,13 @@ void numberOfWays(vector<vector<int>> hatsPerPerson) {
         }
     }
 
-    
     // Create vector for storing results, starting as -1 indicating no results yet on that value
     // dp[i][j], where i = bitmask and j = hatIndex
     vector<vector<int> > dp(1 << people, vector<int>(hatTypes+1, -1));
 
+
     cout << countWays(people, hatTypes, 1, 0, dp, personPerHat, answers, options) << endl; 
-    for(auto i : answers){
+    for (auto i : answers) {
         cout << "(";
         for(int x = 0; x < i.size(); ++x) {
             cout << i[x];
@@ -83,5 +73,4 @@ void numberOfWays(vector<vector<int>> hatsPerPerson) {
         cout << ")" << endl;
     }
 }
-
 
